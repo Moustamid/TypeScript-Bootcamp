23a0cdd3f64ce5e35024c4e156faeb042ef07d04
Moustamid <Karim1992moustamid@gmail.com>
2022-04-15 14:55:48 -0500

    my First TS line .

---
+++ FetchJson/index.ts
@@ -0,0 +1,28 @@
+﻿import axios from 'axios';
+
+const url = "https://jsonplaceholder.typicode.com/todos/10";
+
+interface Todo {
+    id : number;
+    title: string;
+    completed: boolean;
+}
+
+axios.get(url).then( response => {
+    const todo : Todo = response.data as Todo;
+    
+    const id = todo.id;
+    const title = todo.title;
+    const completed = todo.completed;
+    
+    // console.log(response.data);
+    console.log(`
+     The Todo with Id : ${id}
+     Has a title of : ${title}
+     Is it complete ? : ${completed}
+    `);
+});
+
+// .1 tsc index.ts
+// .2 node index.js
+//.3 or ts-node index.ts
